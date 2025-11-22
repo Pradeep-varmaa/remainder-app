9e8475a6c567dbd25c35eb83803c8af54ad8ace1
Pradeep Varma Pothabathula <146648538+Pradeep-varmaa@users.noreply.github.com>
2025-11-22 22:24:16 +0530

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -70,7 +70,7 @@ export default function Home() {
           {
             data.map((el, index)=>{
               console.log(index)
-              const link = el.Link
+              const link = el.Link || ''
               return(
                 <div key={index} className={style.data_subdiv}><p>{index+1}</p> <p>{el.Activity}</p> <a href={link} target='_blank'>LINK</a> <Button onClick={updateStatus}>Complete</Button> </div>
               )
